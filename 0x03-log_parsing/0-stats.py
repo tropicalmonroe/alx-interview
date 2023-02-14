496cf1a1223d28d848b0b06674111455d99a0be5
tropicalmonroe <shitsamajohn821@gmail.com>
2023-02-14 00:09:06 -0800

    Task done fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
-""" a script that reads stdin line by line and computes metrics"""
+""" script that reads stdin line by line and computes metrics """
 
 import sys
 
-def printstate(disc, size):
-    """info print"""
+
+def printStatus(dic, size):
+    """ Prints information """
     print("File size: {:d}".format(size))
-    for x in sorted(disc.keys()):
-        if disc[x] != 0:
-            print("{}: {:d}".format(x, disc[x]))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
 
 # sourcery skip: use-contextlib-suppress
-statuscode = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
@@ -21,7 +23,7 @@ size = 0
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            printstate(statuscode, size)
+            printStatus(statusCodes, size)
 
         stlist = line.split()
         count += 1
@@ -32,13 +34,13 @@ try:
             pass
 
         try:
-            if stlist[-2] in statuscode:
-                statuscode[stlist[-2]] += 1
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
 
-            except Exception:
-                pass
-        printstate(statuscode, size)
 
 except KeyboardInterrupt:
-    printstate(statuscode, size)
+    printStatus(statusCodes, size)
     raise

93f1ad893e1c60336b96ff06c6c10d7d02d13bcc
tropicalmonroe <shitsamajohn821@gmail.com>
2023-02-20 21:07:13 -0800

    UTF-8 done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""
-data set represents a valid UTF-8 encoding
-"""
+"""valid UTF-8 encoding"""
+
 
 def validUTF8(data):
-    """
-    Data: int li
-    Return: True if data is a valid UTF-8 encoding, else return False
-    encoding, else return False
-    """
-
     bytecount = 0
 
-    for x in data:
+    for num in data:
+        binary = format(num, "#010b")[-8:]
         if bytecount == 0:
-            if x >> 5 == 0b110 or x >> 5 == 0b1110:
-                bytecount = 1
-            elif x >> 4 == 0b1110:
-                bytecount = 2
-            elif x >> 3 == 0b11110:
-                bytecount = 3
-            elif x >> 7 == 0b1:
+            for bit in binary:
+                if bit == '0':
+                    break
+                bytecount += 1
+            if bytecount == 0:
+                continue
+            if bytecount == 1 or bytecount > 4:
                 return False
         else:
-            if x >> 6 != 0b10:
+            if binary[0] != '1' or binary[1] != '0':
                 return False
-            bytecount -= 1
-        return bytecount == 0
+
+        bytecount -= 1
+    return bytecount == 0

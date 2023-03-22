2e34047e9924df09a79bc4598cabd9e69506dc9c
tropicalmonroe <shitsamajohn821@gmail.com>
2023-03-22 02:21:51 -0700

    py done fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""fuction that determine the fewest number of coins needed to meet a given amount """
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
 
-def makeChange(coins, total);
-   """takes in list of coins and use that to calc how much total will require"""
-    if total <= 0;
+
+def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
+    if total <= 0:
         return 0
 
     else:

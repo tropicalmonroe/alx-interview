d2019df24f0cbf16544a9397b98269d4458d43d7
tropicalmonroe <shitsamajohn821@gmail.com>
2023-03-30 03:01:30 -0700

    task done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a function def returns the perimeter of the island described in grid
+"""
+
+def island_perimeter(grid):
+    """ the island perimeter func """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter +=4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,8 @@
+i0x09. Island Perimeter
+Algorithm
+Python
+ By: Alexa Orrico, Software Engineer at Holberton School
+ Weight: 1
+ Project will start Mar 27, 2023 6:00 AM, must end by Mar 31, 2023 6:00 AM
+ Checker was released at Mar 28, 2023 6:00 AM
+ An auto review will be launched at the deadline
